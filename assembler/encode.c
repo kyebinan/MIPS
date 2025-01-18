e1abc9a99669ce44edf1451ef47a7a4d1a5ac0d4
Yohan <yohanbinan22@gmail.com>
2025-01-18 17:53:52 +0100

    Working on the encode module Assembler part : Minor Change !

--- assembler/encode.c
+++ assembler/encode.c
@@ -39,26 +39,49 @@ uint32_t encode_J_type(int opcode, int rdest, int imm)
            | (imm_10_1 << 21) | (imm_20 << 31);
 }
 
-
-//TODO : réaliser les fonctions pour les types I, et R
-
 uint32_t encode_I_type(int opcode, int funct3, int rsrc1, int rdest, int imm){
-  //TODO : implementer cette fonction 
+  printf(":: I type (opcode=%d funct3=%d agrs=[%d,%d,%d])\n", opcode, funct3, rsrc1, rdest, imm);
+  int imm_10_0 = (imm & 0b000000000111111111111);
+  return opcode | (rdest << 7) | (funct3 << 12) | (rsrc1 << 15) | (imm_10_0 << 20);
 }
-
 
 uint32_t encode_R_type(int opcode, int funct3, int rsrc1, int rsrc2, int rdest, int imm){
-  //TODO : implementer cette fonction
+  printf(":: R type (opcode=%d funct3=%d agrs=[%d,%d,%d,%d])\n", opcode, funct3, rsrc1, rsrc2, rdest, imm);
+  return opcode | (rdest << 7) | (funct3 << 12) | (rsrc1 << 15) | (rsrc2 << 20) | (imm << 25); 
 }
-
 
 uint32_t encode_instruction(char* name, int arg1, int arg2, int arg3)
 {
   // Cette fonction dispatche les instructions en regardant leur nom.
+  if(!strcmp(name, "add"))
+    return encode_R_type(0b0110011, 0b0, arg1, arg2, arg3, 0b0);
 
-  // Exemple :
+  if(!strcmp(name, "sub"))
+    return encode_R_type(0b0110011, 0b0, arg1, arg2, arg3, 0b00100000);
+
+  if(!strcmp(name, "addi"))
+    return encode_I_type(0b0010011, 0b0, arg1, arg2, arg3);
+
+  if(!strcmp(name, "ld"))
+    return encode_I_type(0b0000011, 0b011, arg1, arg2, arg3);
+  
   if(!strcmp(name, "sd"))
-    return encode_S_type(0b0100011, 0b011, arg1, arg2, arg3);
+    return encode_S_type(0b0100011, 0b011, arg1, arg2, arg3); 
+
+  if(!strcmp(name, "beq"))
+    return encode_B_type(0b01100011, 0b0, arg1, arg2, arg3);
+
+  if(!strcmp(name, "bne"))
+    return encode_B_type(0b01100011, 0b01, arg1, arg2, arg3);
+
+  if(!strcmp(name, "blt"))
+    return encode_B_type(0b01100011, 0b0100, arg1, arg2, arg3);
+
+  if(!strcmp(name, "bge"))
+    return encode_B_type(0b01100011, 0b0101, arg1, arg2, arg3);
+
+  if(!strcmp(name, "jal"))
+    return encode_J_type(0b01101111, arg1, arg2);
 
   //TODO : ajouter les autres instructions.
   // Attention à l'ordre des arguments.
